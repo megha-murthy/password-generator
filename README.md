d2ac03f4ffedaf171073f83fef1d5f11ec6ceefb
Megha Murthy <m.megha21@gmail.com>
2019-12-18 20:33:38 -0800

    files uploaded

--- README.md
+++ README.md
@@ -1,2 +1,36 @@
 # password-generator
-Random Password Generator 
+
+## Description: 
+
+This application generates a random password based on user-selected criteria.
+
+ 
+
+## Usage:
+
+On clicking the Generate Password button, the user will be prompted to choose from the following password criteria:
+
+Length (must be between 8 and 128 characters)
+
+Character type:
+
+·         Special characters
+·         Numeric characters
+·         Lowercase characters
+·         Uppercase characters
+
+Once all prompts are answered, the application validates the user input and ensures that the character type is displayed in the password.
+
+The user will be presented with a password matching the answered prompts.
+
+ 
+
+## Technologies:
+
+HTML 5, CSS 3, Javascript 1.8.5
+
+
+## Screenshots:
+
+![ ](WithoutPassword.png)
+![ ](WithPassword.png)
---
+++ script.js
@@ -0,0 +1,39 @@
+// let button= document.querySelector('button');
+// button.addEventListener('click', function(){
+    
+// });
+
+let button= document.querySelector('button');
+let textArea=document.querySelector('#password');
+
+button.addEventListener('click',function(){
+  let pswdLength= prompt("Enter the length of your password between 8 and 128 charcters");
+  let charType=prompt("Enter a character type");
+
+ let textInput="abcdefghijklmnopqrstuvwxyz0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ~`!#$%^&*+=-[]\\\';,/{}|\":<>?";
+ let textOut="";
+ //let numeric="0123456789"
+ 
+ let len=parseInt(pswdLength);
+
+if(pswdLength==null){
+  alert("Must enter length");
+}
+ if (len<8 || len>128){
+   alert("Please enter a password with length between 8 and 128");
+ }
+
+ if (charType==null){
+   alert("Invalid char type! Please enter a valid char");
+ }
+ if (len>=8 && charType!=null){
+    textOut=textOut+charType;
+    for (let i=0;i<(len)-1; i++){
+      textOut+=textInput.charAt(Math.floor(Math.random() * textInput.length));
+    }
+    alert("Password is Acceptable!!!")
+    textArea.textContent=textOut;
+ }
+
+
+});
